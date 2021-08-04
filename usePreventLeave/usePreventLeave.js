f68dc9d91d4a93c38c3e91d4f26f00bda7f2a852
wonse <sinwonse@gmail.com>
2021-08-04 13:11:10 +0900

    210804

---
+++ usePreventLeave/usePreventLeave.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState, useRef } from "react";
+import ReactDOM from "react-dom";
+
+const usePreventLeave = () => {
+  const listener = (event) => {
+    event.preventDefault();
+    event.returnValue = "";
+  };
+  const enablePrevent = () => window.addEventListener("beforeunload", listener);
+  const disablePrevent = () =>
+    window.removeEventListener("beforeunload", listener);
+  return { enablePrevent, disablePrevent };
+};
+
+const App = () => {
+  const { enablePrevent, disablePrevent } = usePreventLeave();
+  return (
+    <div className="App">
+      <button onClick={enablePrevent}>Protect</button>
+      <button onClick={disablePrevent}>Unprotect</button>
+    </div>
+  );
+};
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);

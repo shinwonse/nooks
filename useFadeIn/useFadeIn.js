abbae6808ad9201db12b865c278a925c0598a569
wonse <sinwonse@gmail.com>
2021-08-07 19:21:28 +0900

    210807

--- useFadeIn/useFadeIn.js
+++ useFadeIn/useFadeIn.js
@@ -18,7 +18,7 @@ const useFadeIn = (duration=1, delay=0) => {
 
 const App = () => {
   const fadeInH1 = useFadeIn(1,2);
-  const fadeInP = useFadeIn(5,10);
+  const fadeInP = useFadeIn(5 ,10);
   return (
     <div className="App">
       <h1 {...fadeInH1}>Hello</h1>
